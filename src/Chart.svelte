edf65826f72248cb9b20e7030f1de5b56596a9db
ythknakamura <ythk.nakamura@gmail.com>
2025-07-01 01:02:09 +0900

    ヒストグラムを見やすく

--- src/Chart.svelte
+++ src/Chart.svelte
@@ -6,14 +6,14 @@
     let {ranking}:{ranking:Result[]} = $props();
     let canvas:HTMLCanvasElement;
     const bins = [
-        {label:"-64~-49", min:-64, max:-49},
-        {label:"-48~-33", min:-48, max:-33},
-        {label:"-32~-17", min:-32, max:-17},
-        {label:"-16~0", min:-16, max:0},
-        {label:"1~16", min:1, max:16},
-        {label:"17~32", min:17, max:32},
-        {label:"33~48", min:33, max:48},
-        {label:"49~64", min:49, max:64}
+        {label:"-64 〜 -49", min:-64, max:-49},
+        {label:"-48 〜 -33", min:-48, max:-33},
+        {label:"-32 〜 -17", min:-32, max:-17},
+        {label:"-16 〜 0", min:-16, max:0},
+        {label:"1 〜 16", min:1, max:16},
+        {label:"17 〜 32", min:17, max:32},
+        {label:"33 〜 48", min:33, max:48},
+        {label:"49 〜 64", min:49, max:64}
     ];
     const frequency = $derived.by(()=>{
         const freq = new Array(bins.length).fill(0);
@@ -36,7 +36,10 @@
                 datasets: [{
                     label: '弱さ',
                     data: frequency,
-                    borderWidth: 1,
+                    borderWidth: 0.5,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0,
+                    borderColor: '#eeeeee',
                     backgroundColor: bins.map( b => b.min>0?'#36A2EB':'#FF6384'),
                 }]
             },
@@ -48,13 +51,16 @@
                     },
                     tooltip:{
                         enabled: false,
-                    }
+                    },
                 },
                 scales:{
                     x:{
                         title:{
                             display: true,
                             text: '人数'
+                        },
+                        grid: {
+                            display: true,
                         },
                         beginAtZero: true,
                     },
@@ -63,6 +69,9 @@
                         title:{
                             display: true,
                             text: '弱さ'
+                        },
+                        grid: {
+                            display: true,
                         }
                     }
                 }
@@ -73,5 +82,5 @@
 
 <Card class="bg-gray-50 shadow-lg rounded-2xl py-4 pl-4 pr-6 max-w-full">
     <h2 class="text-xl font-bold mb-2">弱さの度数分布</h2>
-    <canvas bind:this={canvas} width="100%" height="80%"></canvas>
+    <canvas bind:this={canvas} width="100%" height="70%"></canvas>
 </Card>
