eb8a32f20545e149bcedb6f5954c7c2d333297a1
ythknakamura <ythk.nakamura@gmail.com>
2025-07-05 10:27:34 +0900

    サイズ微調整

--- src/Ranking.svelte
+++ src/Ranking.svelte
@@ -7,7 +7,7 @@
     );
 </script>
 
-<div class="h-[43%]">
+<div class="h-[50%]">
 <Card class="bg-gray-50 shadow-lg rounded-2xl py-4 px-8 max-w-full max-h-full overflow-scroll">
     <h2 class="text-xl font-bold mb-2">最弱のプレーヤー</h2>
     <Table striped border={false}>
